29232b79700a475b4847283ebc2416294f7b3867
beraer <s29315@pjwstk.edu.pl>
2025-03-13 00:25:48 +0100

    FlashcardsApp(main class) was added

--- src/main/java/tpo02/s29315tpo02/FlashcardsApp.java
+++ src/main/java/tpo02/s29315tpo02/FlashcardsApp.java
@@ -1,7 +1,13 @@
 package tpo02.s29315tpo02;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import tpo02.s29315tpo02.controller.FlashcardsController;
+
 public class FlashcardsApp {
     public static void main(String[] args) {
-
+        ConfigurableApplicationContext context = SpringApplication.run(FlashcardsApp.class, args);
+        FlashcardsController controller = context.getBean(FlashcardsController.class);
+        controller.run();
     }
 }
--- src/main/java/tpo02/s29315tpo02/config/FlashcardsProperties.java
+++ src/main/java/tpo02/s29315tpo02/config/FlashcardsProperties.java
@@ -1,4 +1,7 @@
 package tpo02.s29315tpo02.config;
 
+import org.springframework.context.ConfigurableApplicationContext;
+
 public class FlashcardsProperties {
+
 }
