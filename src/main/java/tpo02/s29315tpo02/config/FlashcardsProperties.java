1b035ae72e0d3751774a18bba68bb9fe3c02faa9
beraer <s29315@pjwstk.edu.pl>
2025-03-13 01:03:00 +0100

    FlashcardProperties class is edited for reading updated properties from external property pl.edu.pja.tpo02

--- src/main/java/tpo02/s29315tpo02/config/FlashcardsProperties.java
+++ src/main/java/tpo02/s29315tpo02/config/FlashcardsProperties.java
@@ -1,7 +1,13 @@
 package tpo02.s29315tpo02.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
+@ConfigurationProperties(prefix = "pl.edu.pja.tpo02")
 public class FlashcardsProperties {
-
+    private String filename;
+    public String getFilename() {return filename;}
+    public void setFilename(String filename) {this.filename = filename;}
 }
